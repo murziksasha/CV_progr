8b1763c9021faf8b9673390807a32e67ab9dc9e2
Alexander Grygoriev <truedevelop@yahoo.com>
2022-05-25 15:33:29 +0300

    about me section done

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded",()=>{
+  const menuBlock = document.querySelector('.menu');
+  const menuClose= document.querySelector('.menu__close');
+  const hamburger = document.querySelector('.hamburger');
+
+  // hamburger.addEventListener('click',()=>{
+  //     menuBlock.classList.add('active');
+  //     menuClose.addEventListener('click', ()=>{
+  //     menuBlock.classList.remove('active');
+  //   })
+  // })
+
+
+})
