c9025b74eb4c2a3c7a157e200f659053a4f131fb
Alexander Grygoriev <truedevelop@yahoo.com>
2022-06-09 15:11:58 +0300

    adaptive changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,5 +20,33 @@ window.addEventListener("DOMContentLoaded",()=>{
     factRatioLine[i].style.width = item.innerHTML;
   });
 
+  //array to UP 
+  (function() {
+    'use strict';
+  
+    function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+  
+      if (scrolled > coords) {
+        goTopBtn.classList.add('back_to_top-show');
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.remove('back_to_top-show');
+      }
+    }
+  
+    function backToTop() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+      }
+    }
+  
+    var goTopBtn = document.querySelector('.back_to_top');
+  
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+  })();
 
 })
